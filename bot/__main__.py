473adb4a26011aad7789b50cd870f88a20211bfa
KekovD <lavrenov_d@niuitmo.ru>
2024-05-20 22:30:58 +0300

    refactor: add privacy mode, need fix mention in groups

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,15 +1,38 @@
 import threading
 import signal
 import sys
+
+from telegram import Bot, MessageEntity
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram_bot import start, handle_message, stop
-from config import TOKEN
+from config import TOKEN, BOT_USERNAME
 from fs_utils import start_fuse, unmount_fs
 
 
 def signal_handler(sig, frame):
     unmount_fs()
     sys.exit(0)
+
+
+def handle_private(update, context):
+    message_text = update.message.text
+    if message_text == '/stop':
+        stop(update, context)
+    elif message_text == '/start':
+        start(update, context)
+
+
+# TODO: fix group mention
+def handle_mention(update, context):
+    entities = update.message.parse_entities([MessageEntity.MENTION])
+    for entity in entities.values():
+        if entity.lower() == BOT_USERNAME.lower():
+            message_text = update.message.text
+            if '/start' in message_text:
+                start(update, context)
+            elif '/stop' in message_text:
+                stop(update, context)
+
 
 
 def main():
@@ -22,9 +45,10 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("stop", stop))
-    dp.add_handler(MessageHandler(Filters.document, handle_message))
+    dp.add_handler(MessageHandler(Filters.text & Filters.chat_type.private, handle_private))
+    dp.add_handler(MessageHandler(Filters.document & Filters.chat_type.private, handle_message))
+
+    dp.add_handler(MessageHandler(Filters.entity(MessageEntity.MENTION), handle_mention))
 
     updater.start_polling()
     updater.idle()
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from dotenv import load_dotenv
+from telegram import Bot
 
 load_dotenv()
 
@@ -12,3 +13,4 @@ logger = logging.getLogger(__name__)
 TOKEN = os.getenv('TOKEN')
 MOUNT_POINT = os.getenv('MOUNT_POINT')
 STORAGE_PATH = os.getenv('STORAGE_PATH')
+BOT_USERNAME = Bot(TOKEN).get_me().username
