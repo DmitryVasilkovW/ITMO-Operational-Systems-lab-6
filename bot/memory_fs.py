36ab38fa33bb8dc415ab473b9efb1a625517e408
KekovD <lavrenov_d@niuitmo.ru>
2024-05-25 22:04:23 +0300

    refactor: remove async in memory_fs.py
    Signed-off-by: KekovD <lavrenov_d@niuitmo.ru>

--- bot/memory_fs.py
+++ bot/memory_fs.py
@@ -15,13 +15,13 @@ class MemoryFS(Operations):
         now = time.time()
         self.files['/'] = dict(st_mode=(stat.S_IFDIR | 0o755), st_ctime=now, st_mtime=now, st_atime=now, st_nlink=2)
 
-    async def getattr(self, inode, ctx=None):
+    def getattr(self, inode, ctx=None):
         path = llfuse.fuse_decode_inode(inode)
         if path not in self.files:
             raise FUSEError(errno.ENOENT)
         return self.files[path]
 
-    async def lookup(self, parent_inode, name, ctx=None):
+    def lookup(self, parent_inode, name, ctx=None):
         path = llfuse.fuse_decode_inode(parent_inode)
         name = name.decode()
         full_path = os.path.join(path, name)
@@ -29,14 +29,14 @@ class MemoryFS(Operations):
             raise FUSEError(errno.ENOENT)
         return self.files[full_path]
 
-    async def readdir(self, inode, off, token):
+    def readdir(self, inode, off, token):
         path = llfuse.fuse_decode_inode(inode)
         entries = ['.', '..'] + [x[1:] for x in self.files if x != '/' and x.startswith(path)]
         for i, entry in enumerate(entries):
             if i >= off:
                 llfuse.readdir_add(token, entry.encode('utf-8'), llfuse.ROOT_INODE + i + 1, off + 1)
 
-    async def mknod(self, parent_inode, name, mode, rdev, ctx=None):
+    def mknod(self, parent_inode, name, mode, rdev, ctx=None):
         path = llfuse.fuse_decode_inode(parent_inode)
         name = name.decode()
         full_path = os.path.join(path, name)
@@ -44,7 +44,7 @@ class MemoryFS(Operations):
                                      st_ctime=time.time(), st_mtime=time.time(), st_atime=time.time())
         self.data[full_path] = b''
 
-    async def mkdir(self, parent_inode, name, mode, ctx=None):
+    def mkdir(self, parent_inode, name, mode, ctx=None):
         path = llfuse.fuse_decode_inode(parent_inode)
         name = name.decode()
         full_path = os.path.join(path, name)
@@ -52,25 +52,25 @@ class MemoryFS(Operations):
                                      st_ctime=time.time(), st_mtime=time.time(), st_atime=time.time())
         self.files['/']['st_nlink'] += 1
 
-    async def unlink(self, parent_inode, name, ctx=None):
+    def unlink(self, parent_inode, name, ctx=None):
         path = llfuse.fuse_decode_inode(parent_inode)
         name = name.decode()
         full_path = os.path.join(path, name)
         self.files.pop(full_path)
         self.data.pop(full_path, None)
 
-    async def rmdir(self, parent_inode, name, ctx=None):
+    def rmdir(self, parent_inode, name, ctx=None):
         path = llfuse.fuse_decode_inode(parent_inode)
         name = name.decode()
         full_path = os.path.join(path, name)
         self.files.pop(full_path)
         self.files['/']['st_nlink'] -= 1
 
-    async def read(self, fh, off, size):
+    def read(self, fh, off, size):
         path = llfuse.fuse_decode_inode(fh)
         return self.data[path][off:off + size]
 
-    async def write(self, fh, off, buf):
+    def write(self, fh, off, buf):
         path = llfuse.fuse_decode_inode(fh)
         if path not in self.data:
             self.data[path] = b''
@@ -78,7 +78,7 @@ class MemoryFS(Operations):
         self.files[path]['st_size'] = len(self.data[path])
         return len(buf)
 
-    async def setattr(self, inode, attr, fields, ctx):
+    def setattr(self, inode, attr, fields, ctx):
         path = llfuse.fuse_decode_inode(inode)
         if path not in self.files:
             raise FUSEError(errno.ENOENT)
