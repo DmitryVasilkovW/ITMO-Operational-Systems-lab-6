2de265d7a0db77bc488055bc33db326dafeaccd3
KekovD <lavrenov_d@niuitmo.ru>
2024-05-30 22:07:24 +0300

    refactor: codestyle
    Signed-off-by: KekovD <lavrenov_d@niuitmo.ru>

--- bot/converter.py
+++ bot/converter.py
@@ -1,10 +1,13 @@
 from PIL import Image
 
+
 def convert_png_to_jpg(png_path):
     img = Image.open(png_path)
     rgb_im = img.convert('RGB')
     rgb_im.save(png_path[:-3] + 'jpg')
 
+
 def create_empty_jpg(png_path):
     jpg_path = png_path[:-3] + 'jpg'
     open(jpg_path, 'a').close()
+    
